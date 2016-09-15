baefc3b19726153e051af07a01cf0f4b993dbb71
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-09-15 09:20:56 +0200

    feat: easy access to parsed and filtered data.

---
+++ marsi/io/data.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Chr. Hansen A/S and The Novo Nordisk Foundation Center for Biosustainability, DTU.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from marsi.utils import data_dir
+from pandas import DataFrame
+
+__all__ = ["chebi", "drugbank"]
+
+chebi = DataFrame.from_csv(os.path.join(data_dir, "chebi_analogues_filtered.csv"))
+
+drugbank = DataFrame.from_csv(os.path.join(data_dir, "drugbank_vocabulary_5.0.1.csv"), sep=",", index_col=None)
+drugbank.columns = ["id", "accessions", "common_names", "cas", "unii", "synonyms", "inchi_key"]
+
+drugbank.fillna("", inplace=True)
+
+drugbank['accessions'] = drugbank.accessions.apply(str.split, args=(" | ",))
+drugbank['synonyms'] = drugbank.synonyms.apply(str.split, args=(" | ",))
+
+pubchem = DataFrame.from_csv(os.path.join(data_dir, "pubchem_data.csv"))
+
+kegg = DataFrame.from_csv(os.path.join(data_dir, "kegg_data.csv"))
