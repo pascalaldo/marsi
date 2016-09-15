9501d5aaf3055bd3f6185b6bb44c6105741ceaa0
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-09-15 15:34:13 +0200

    fix: except invalid inchi strings

--- marsi/design/operations.py
+++ marsi/design/operations.py
@@ -11,7 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from numpy import array
 
+from marsi.distances import tanimoto_distance
+from marsi.processing.models import find_bigg_metabolite
+from marsi.processing.chemistry import inchi_to_molecule
 
 __all__ = ['NearestNeighbors']
 
@@ -20,12 +24,18 @@ class NearestNeighbors(object):
     def __init__(self, database):
         self.database = database
 
-    def __call__(self, metabolite, tanimoto_distance=0.8, rmsd_distance=0.8):
-        inchi, inchi_key = find_model_metabolite(metabolite.id)
+    def __call__(self, metabolite, min_tanimoto=0.8, min_rmsd=0.8, fpformat='maccs'):
+        try:
+            inchi, inchi_key = find_bigg_metabolite(metabolite.model, metabolite.id)
+            metabolite_molecule = inchi_to_molecule(inchi)
+            metabolite_fingerprint = metabolite_molecule.calcfp(fpformat)
+            fingerprints = [m.fingerprint(fpformat=fpformat) for m in self.database.metabolites]
+            tamimoto_distances = array([tanimoto_distance(metabolite_fingerprint, fp) for fp in fingerprints])
+            tier1 = tamimoto_distances[tamimoto_distances < min_tanimoto]
 
-        tamimoto_distances = self.database.metabolites.inchi.apply(tanimoto, args=(inchi, ))
-        tier1 = tamimoto_distances[tamimoto_distances.distance < tanimoto_distance]
+            # rmsd_distances = tier1.apply(rsmd, args=(inchi, ))
+            # tier2 = rmsd_distances[rmsd_distances < rmsd_distance]
+            return inchi, inchi_key, tier1
+        except ValueError:
+            return None, None, []
 
-        rmsd_distances = tier1.apply(rsmd, args=(inchi, ))
-        tier2 = rmsd_distances[rmsd_distances < rmsd_distance]
-        return inchi, inchi_key, tier2
