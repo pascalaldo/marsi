7fb587f6e41a5c435ee34ff7d797715e1789edb8
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-03-23 15:42:16 +0100

    chore: documentation and representation

--- marsi/cobra/strain_design/target.py
+++ marsi/cobra/strain_design/target.py
@@ -14,7 +14,8 @@
 
 from cameo.core.target import Target
 
-from marsi.cobra.flux_analysis.manipulation import knockout_metabolite
+from marsi.cobra.flux_analysis.manipulation import knockout_metabolite, inhibit_metabolite, compete_metabolite, \
+    apply_anti_metabolite
 from marsi.utils import search_metabolites
 
 
@@ -38,25 +39,53 @@ class AntiMetaboliteManipulationTarget(Target):
         self.allow_accumulation = allow_accumulation
 
     def metabolites(self, model):
+        """
+        Finds the metabolites in the model across compartments.
+
+        Parameters
+        ----------
+        model: cameo.SolverBasedModel
+            A constraint-based model.
+        Returns
+        -------
+        list
+            A list of cameo.core.Metabolite.
+        """
         return search_metabolites(model, self.id)
 
-    def apply(self, model, time_machine=None):
-        pass
+    def apply(self, model, time_machine=None, reference=None):
+        essential_metabolites = model.essential_metabolites()
+        target_metabolites = self.metabolites(model)
+
+        apply_anti_metabolite(model, target_metabolites, essential_metabolites, reference,
+                              allow_accumulation=self.allow_accumulation,
+                              competition_fraction=self.fraction,
+                              inhibition_fraction=self.fraction,
+                              time_machine=time_machine)
+
+    def _repr_html_(self):  # pragma: no cover
+        return "&#x2623;(%.3f)-%s" % (self.fraction, self.id)
 
     def __str__(self):
-        return "X(%.3f)-%s" % (self.fraction, self.id)
+        return b'\xe2\x98\xa3'.decode('utf-8') + "(%.3f)-%s" % (self.fraction, self.id)
 
     def __repr__(self):
-        return str(self)
+        return "<AntiMetaboliteManipulation %s (%.3f)>" % (self.id, self.fraction)
 
 
 class MetaboliteKnockoutTarget(AntiMetaboliteManipulationTarget):
     def __init__(self, species_id, ignore_transport=True, allow_accumulation=True):
         super(MetaboliteKnockoutTarget, self).__init__(species_id, 0.0, ignore_transport, allow_accumulation)
 
-    def apply(self, model, time_machine=None):
+    def apply(self, model, time_machine=None, reference=None):
         for metabolite in self.metabolites(model):
-            knockout_metabolite(metabolite, self.ignore_transport, self.allow_accumulation, time_machine)
+            knockout_metabolite(model, metabolite, self.ignore_transport, self.allow_accumulation, time_machine)
 
     def __str__(self):
-        return "X-%s" % self.id
+        return b'\xe2\x98\xa3'.decode('utf-8') + "-%s" % self.id
+
+    def __repr__(self):
+        return "<MetaboliteKnockout %s>" % self.id
+
+    def _repr_html_(self):  # pragma: no cover
+        return "&#x2623;%s" % self.id
