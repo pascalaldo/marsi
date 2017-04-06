a1c572b557b6fe5b94186763210f3bfa15f3e660
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-04-06 16:03:12 +0200

    chore: quick start

--- marsi/io/__init__.py
+++ marsi/io/__init__.py
@@ -11,3 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import xlwt
+
+def write_excel_file(data_frame, path, molecules):
+    pass
