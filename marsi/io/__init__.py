8a3b26d25e4704eb492f041afb3868adc91e9d9d
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-04-07 09:59:25 +0200

    fix: not using xlwt

--- marsi/io/__init__.py
+++ marsi/io/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import xlwt
 
 def write_excel_file(data_frame, path, molecules):
     pass
