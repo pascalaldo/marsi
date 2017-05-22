aac0d997e089f0a7bccd63b11578eff126b0b493
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-05-22 16:49:48 +0200

    fix: flake8

--- marsi/cli/controllers/chemistry.py
+++ marsi/cli/controllers/chemistry.py
@@ -11,21 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from numbers import Number
-
-import os
 from cement.core.controller import CementBaseController, expose
-from pymongo.errors import ServerSelectionTimeoutError
 
-from marsi import config, utils
-from marsi.nearest_neighbors import build_feature_table, search_closest_compounds
+from marsi import config
+from marsi.nearest_neighbors import search_closest_compounds
 from marsi.chemistry.molecule import Molecule
 from mongoengine import connect
 
 from marsi.io import write_excel_file
-
-
-from marsi.utils import pickle_large
 
 OUTPUT_WRITERS = {
     'csv': lambda df, path, *args: df.to_csv(path),
@@ -54,6 +47,9 @@ class ChemistryController(CementBaseController):
             (['--radius', '-r'], dict(help="Filter hits within R distance radius")),
             (['--atoms-weight', '-aw'], dict(help="The weight of the atoms for structural similarity")),
             (['--bonds-weight', '-bw'], dict(help="The weight of the bonds for structural similarity")),
+            (['--atoms-diff', '-ad'], dict(help="The maximum number of the atoms difference for database query")),
+            (['--bonds-diff', '-bd'], dict(help="The maximum number of the bonds difference for database query")),
+            (['--rings-diff', '-rd'], dict(help="The maximum number of the rings difference for database query")),
             (['--output-file', '-o'], dict(help="Output file")),
             (['--output-format', '-f'], dict(help='Output format (default: csv)', default='csv'))
         ]
@@ -63,42 +59,6 @@ class ChemistryController(CementBaseController):
         print("Welcome to MARSI chemistry package")
         print("Here you can find the tools to find and sort analogs for metabolites")
 
-    @expose(help="Build a nearest neighbors model with the specified fingerprints")
-    def nearest_neighbors_model(self):
-        connect(config.db_name)
-        fpformat = self.app.pargs.fingerprint_format
-        if fpformat not in pybel.fps:
-            print("Invalid 'fingerprint-format' %s. Use of of %s" % (fpformat, ", ".join(pybel.fps)))
-
-        solubility = "high"
-
-        print("This process will take several hours, please confirm the following information:")
-        print("+--------------------+---------------+")
-        print("| Fingerprint format |        %s |" % fpformat.rjust(6))
-        print("+--------------------+---------------+")
-        print("|         Solubility |        %s |" % solubility.rjust(6))
-        print("+--------------------+---------------+")
-
-        value = input("Do you want to continue y/n [y]: ")
-
-        if len(value) == 0:
-            value = "y"
-
-        if value != "y":
-            print("Cancelled")
-            exit(1)
-
-        model_file = os.path.join(utils.data_dir, "fingerprints_default_%s_sol_%s.pickle" % (fpformat, solubility))
-        print("File will be saved as %s" % model_file)
-
-        try:
-            indices, features, lengths = build_feature_table(Database.metabolites, fpformat=fpformat,
-                                                             solubility=solubility, chunk_size=1e5)
-            pickle_large((indices, features, lengths), model_file, progress=True)
-            print("Completed")
-        except ServerSelectionTimeoutError:
-            print("Error: cannot connect to database. Make sure your mongodb is running and accessible.")
-            exit(1)
 
     @expose(help="Find analogs for a metabolite")
     def find_analogs(self):
@@ -125,6 +85,9 @@ class ChemistryController(CementBaseController):
 
         bonds_weight = 0.5
         atoms_weight = 0.5
+        bonds_diff = 3
+        atoms_diff = 4
+        rings_diff = 3
 
         if self.app.pargs.bonds_weight is not None:
             bonds_weight = float(self.app.pargs.bonds_weight)
@@ -132,13 +95,20 @@ class ChemistryController(CementBaseController):
         if self.app.pargs.atoms_weight is not None:
             atoms_weight = float(self.app.pargs.atoms_weight)
 
+        if self.app.pargs.bonds_diff is not None:
+            bonds_diff = float(self.app.pargs.bonds_diff)
+
+        if self.app.pargs.atoms_diff is not None:
+            atoms_diff = float(self.app.pargs.atoms_diff)
+
+        if self.app.pargs.rings_diff is not None:
+            rings_diff = float(self.app.pargs.rings_diff)
+
         if self.app.pargs.inchi is not None:
             molecule = Molecule.from_inchi(self.app.pargs.inchi)
         elif self.app.pargs.sdf is not None:
-            use_volume = True
             molecule = Molecule.from_sdf(self.app.pargs.sdf)
         elif self.app.pargs.mol is not None:
-            use_volume = True
             molecule = Molecule.from_mol(self.app.pargs.mol)
         else:
             print("Please provide one of the following inputs --inchi, --sdf or --mol")
@@ -147,6 +117,7 @@ class ChemistryController(CementBaseController):
         fpformat = self.app.pargs.fingerprint_format
 
         results = search_closest_compounds(molecule=molecule, fpformat=fpformat, bonds_weight=bonds_weight,
-                                           bonds_diff=bonds_diff, atoms_weight=atoms_weight, atoms_diff=atoms_diff)
+                                           bonds_diff=bonds_diff, atoms_weight=atoms_weight, atoms_diff=atoms_diff,
+                                           rings_diff=rings_diff)
 
         OUTPUT_WRITERS[self.app.pargs.output_format](results, output_file, None)
