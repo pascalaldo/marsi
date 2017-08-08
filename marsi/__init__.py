e2ce14e04ef10d475e19f24c2e08b2078f11cdda
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-08-08 16:35:10 +0200

    fix: everything is unicode

--- marsi/__init__.py
+++ marsi/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
 from marsi import config
 from ._version import get_versions
