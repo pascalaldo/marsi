7446432bdcd58b52a580967944cd2ece129c6b5b
Joao Cardoso <joaca@biosustain.dtu.dk>
2017-08-27 21:11:21 +0200

    fix: clear imports

--- marsi/cobra/strain_design/design_methods.py
+++ marsi/cobra/strain_design/design_methods.py
@@ -17,8 +17,7 @@ from __future__ import absolute_import
 from cameo.core.utils import get_reaction_for
 from cameo.flux_analysis.analysis import find_essential_metabolites
 from cameo.flux_analysis.simulation import fba
-from cameo.strain_design import DifferentialFVA
-from cameo.strain_design import OptKnock, OptGene
+from cameo.strain_design import OptKnock, OptGene, DifferentialFVA
 from cameo.strain_design.heuristic.evolutionary.objective_functions import biomass_product_coupled_yield
 from cobra.core.model import Model
 from pandas import DataFrame
